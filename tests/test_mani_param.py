e075c2ec8e614cdfd34986a91350e73fc8932230
HYGOR VINICIUS COSTA SILVA <hygorvcosta@gmail.com>
2023-05-03 05:18:18 -0300

    Refactor: Auto-pep8

--- tests/test_mani_param.py
+++ tests/test_mani_param.py
@@ -1,18 +1,27 @@
-import pytest
+""" Test Mani Param - PyMEX Class"""
 import os
+from unittest import mock
+
 import pandas as pd
+import pytest
+
 from res_pymex import PyMEX
-import unittest.mock as mock
+
 
 @pytest.fixture
 def pymex():
+    """Create instance of Pymex to test.
+    """
     controls = [1] * 58
-    return PyMEX(controls)
+    return PyMEX(controls, 'example/config.yaml')
 
-def test_run_imex(pymex):
-    pymex()
 
 def test_run_results_report(pymex):
+    """Teste if production is not none.
+
+    Args:
+        pymex (instance of PyMEX)
+    """
     procedure = mock.MagicMock()
     procedure.returncode = 0
     pymex.run_report_results(procedure)
@@ -20,12 +29,16 @@ def test_run_results_report(pymex):
 
 
 def test_cash_flow(pymex):
+    """Test if clashflow was created and if is a pandas Series.
+    """
     pymex.prod = pd.read_csv('producao.csv')
     cash_flow = pymex.cash_flow()
     assert len(cash_flow) == 361
     assert isinstance(cash_flow, pd.Series)
 
 def test_npv(pymex):
+    """Test if vpl is positive by default.
+    """
     pymex.prod = pd.read_csv('producao.csv')
     pymex.net_present_value()
     assert isinstance(pymex.npv, float)
@@ -33,10 +46,12 @@ def test_npv(pymex):
 
 
 def test_clean_up(pymex):
+    """Test if clean up temporary works is working.
+    """
     pymex.clean_up()
-    assert os.path.exists(pymex.basename.dat) == False
-    assert os.path.exists(pymex.basename.sr3) == False
-    assert os.path.exists(pymex.basename.out) == False
-    assert os.path.exists(pymex.basename.rwo) == False
-    assert os.path.exists(pymex.basename.rwd) == False
-    assert os.path.exists(pymex.basename.log) == False
+    assert os.path.exists(pymex.basename.dat) is False
+    assert os.path.exists(pymex.basename.sr3) is False
+    assert os.path.exists(pymex.basename.out) is False
+    assert os.path.exists(pymex.basename.rwo) is False
+    assert os.path.exists(pymex.basename.rwd) is False
+    assert os.path.exists(pymex.basename.log) is False
