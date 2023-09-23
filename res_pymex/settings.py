29b5ba9aee6e00b736ac06d5e0a953be429d5f31
Hygor Costa <hygor.vinicius@petrobras.com.br>
2023-09-23 15:37:15 -0300

    Add results in model paths

--- res_pymex/settings.py
+++ res_pymex/settings.py
@@ -11,6 +11,7 @@ class Model():
         self.tpl = ""
         self.basename = ""
         self.temp_run = ""
+        self.results = ""
         self.tpl_report = ""
 
 class Wells():
@@ -54,6 +55,8 @@ class Settings:
         new_model.tpl = Path(data_yaml['dat_tpl'])
         new_model.temp_run = Path(data_yaml['temp_run'])
         new_model.temp_run.mkdir(exist_ok=True)
+        new_model.results = Path(data_yaml['results'])
+        new_model.results.mkdir(exist_ok=True)
         new_model.basename = cmgfile(new_model.temp_run / new_model.tpl.name)
         new_model.tpl_report = Path(data_yaml['report_tpl'])
         return new_model
